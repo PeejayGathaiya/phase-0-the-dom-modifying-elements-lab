63c471a507e451caa54aea9c53b2dbeb6a1865ef
Peejay Gathaiya <gathaiya.peejay@gmail.com>
2023-10-12 15:02:08 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+// let newHeader = document.createElement("h1");
+// document.newHeader.append('victory')
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+
+
+
+// const newHeader = document.createElement("h1")
+// const id = newHeader.createElement('id')
+// id.textContent = 'victory'
+//newHeader.innerText = 'YOUR-NAME is the champion'
+
+
